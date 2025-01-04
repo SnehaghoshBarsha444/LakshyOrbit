28a2f7f6edf8756bd6afc1df4b1b53883bd8658a
Sneha Ghosh (Technical_Isopod_075) <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2025-01-04 13:33:35 +0530

    Delete Kotlin/LakshyOrbit-Kotlin-master/app/src/main/java directory

--- Kotlin/LakshyOrbit-Kotlin-master/app/src/main/java/com/harshrajpurohit/letsbrowse/model/Bookmark.kt
+++
@@ -1,3 +0,0 @@
-package com.snehaghoshTechIsopod.LakshyOrbit.model
-
-data class Bookmark(val name: String, val url: String, var image: ByteArray? = null, var imagePath: Int? = null)
--- Kotlin/LakshyOrbit-Kotlin-master/app/src/main/java/com/harshrajpurohit/letsbrowse/model/Tab.kt
+++
@@ -1,5 +0,0 @@
-package com.snehaghoshTechIsopod.LakshyOrbit.model
-
-import androidx.fragment.app.Fragment
-
-data class Tab(var name: String, val fragment: Fragment)
